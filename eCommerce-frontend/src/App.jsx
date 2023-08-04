5b30b0d8d42783bddbe9bd9903d22b5e06d6b658
unknown <lalit4283@gmail.com>
2023-08-04 10:40:52 +0530

    products layout created

--- eCommerce-frontend/src/App.jsx
+++ eCommerce-frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import ProductList from "./features/productList/ProductList"
-
-
+import './index.css'
 function App() {
 
   return (
     <>
+    <h1 className=" text-cyan-500">Hello</h1>
       <ProductList></ProductList>
     </>
   )
